0d8eab46dfd4aeea9294cc865b046bf00e4c42a8
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-09-21 17:22:57 +0100

    Fix: TDD | Adding wait_for_db() functionality to fix race condition with other services
    - Added test case and functionality to make sure we are waiting for database to start completely and holding other services until the application is launched. This is done so that we don't face race condition between different docker image applications and databases. If race condition happens there will be error and the application will not launch.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,29 @@
+"""
+Django command to wait for the database to be available.
+"""
+
+import time
+
+from psycopg2 import OperationalError as Psycopg20pError
+
+from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command"""
+        self.stdout.write("Wait for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
+                self.stdout.writ("Database unavailable\n"
+                                 "Waiting for 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
